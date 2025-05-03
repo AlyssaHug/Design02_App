9267e7ae5bef96990c34c279f742e7f13b9e48c1
tc.11927 <tcalaquian2@my.bcit.ca>
2025-05-03 14:02:43 -0700

    first commit

--- app/page.js
+++ app/page.js
@@ -3,13 +3,14 @@
 import Nav from "@/app/ui/navbar/navbar";
 import Slider from "@/app/ui/slider/slider";
 import Dressup from "./ui/dressup/dressup";
+import Goalscarousel from "@/app/ui/goalscarousel/goalscarousel";
 
 function Page() {
     return (
         <>
- 
             <Slider />
             <Dressup/>
+            <Goalscarousel/>
         </>
     );
 }
