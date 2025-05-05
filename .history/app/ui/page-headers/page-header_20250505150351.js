851af4d6bb9e9d846334ae3e41438685485e9825
Darrel Soriano <dsoriano5@my.bcit.ca>
2025-05-05 15:11:53 -0700

    Started Page-Header js & css

---
+++ .history/app/ui/page-headers/page-header_20250505150351.js
@@ -0,0 +1,3 @@
+import classNames from "classnames";
+import { Libre_Franklin } from "next/font/google";
+import styles from "@/app/ui/page-headers/page-header.module.css";
---
+++ .history/app/ui/page-headers/page-header_20250505150414.js
@@ -0,0 +1,8 @@
+import classNames from "classnames";
+import { Libre_Franklin } from "next/font/google";
+import styles from "@/app/ui/page-headers/page-header.module.css";
+
+const libreFranklin = Libre_Franklin({
+    variable: "--font-libre-franklin",
+});
+
