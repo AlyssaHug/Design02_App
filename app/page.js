9e33e8d97f8c51d757fe168626750cb4c79122b8
tc.11927 <tcalaquian2@my.bcit.ca>
2025-05-05 15:12:04 -0700

    slider movement

--- app/page.js
+++ app/page.js
@@ -3,11 +3,12 @@
 import Nav from "@/app/ui/navbar/navbar";
 import Slider from "@/app/ui/slider/slider";
 
+
 function Page() {
     return (
         <>
+ <Slider></Slider>
  
-            <Slider />
         </>
     );
 }
