c16491e332d2c02332a9d11c10295b6ba6eeb94c
tc.11927 <tcalaquian2@my.bcit.ca>
2025-05-05 14:35:32 -0700

    fixed navbar at bottom

--- app/page.js
+++ app/page.js
@@ -1,9 +1,23 @@
 "use client";
+import GoalProgress from "./ui/GoalCards/GoalCards";
 
 import Nav from "@/app/ui/navbar/navbar";
 function page() {
     return (
+        <div>
        <Nav/>
+       <GoalProgress/>
+       <GoalProgress/>
+       <GoalProgress/>
+       <GoalProgress/>
+       <GoalProgress/>
+       <GoalProgress/>
+       <GoalProgress/>
+       <GoalProgress/>
+       <GoalProgress/>
+       <GoalProgress/>
+       <GoalProgress/>
+       </div>
     );
 }
 
