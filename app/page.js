12ca30f8ea52dcca33eae2dcb20e19704428d6c7
tc.11927 <tcalaquian2@my.bcit.ca>
2025-05-06 11:17:07 -0700

    added background

--- app/page.js
+++ app/page.js
@@ -1,23 +1,28 @@
 "use client";
 import Coins from "./ui/coins/coins";
-import Dropdown from "@/app/ui/dropdown/dropdown";
-import ToggleButton from "@/app/ui/Buttons/ToggleButton";
-import GoalProgress from "@/app/ui/GoalCards/GoalCards";
-import { Goals } from "@/app/ui/GoalCards/GoalCards";
-import { GoalRecs } from "@/app/ui/GoalCards/GoalCards";
-import styles from "@/app/ui/GoalCards/GoalCards.module.css";
-import Button from "@/app/ui/Buttons/Buttons";
-import { GoalCow } from "@/app/ui/GoalCards/GoalCards";
-import TransactionCard from "@/app/ui/TransactionCards/transactionCard";
-import CalendarCard from "@/app/ui/CalendarCard/calendarCard";
-
 import Nav from "./ui/navbar/navbar";
+
+import styles from "@/app/Cow/styles.module.css";
+
 export default function Page() {
     return (
-      <div>
-        
-      </div>
-      );
-    }
-
-
+        <div className={styles.container}>
+            <Nav />
+            <div className={styles.sameRow}>
+                <img
+                    src='/left_arrow.svg'
+                    alt='back arrow'
+                />
+                <div className={styles.coins}>
+                    <Coins
+                        value='Coins: '
+                        coin={990}
+                    />
+                </div>
+            </div>
+            <div className={styles.background}>
+                <img src="/grassbg.svg" className={styles.backgroundImage} alt="grass background" />
+            </div>
+        </div>
+    );
+}
