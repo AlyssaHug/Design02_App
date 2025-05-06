de2d271dd596dd88cb00f1e86a02c4a1fbfa5cca
tc.11927 <tcalaquian2@my.bcit.ca>
2025-05-06 14:25:51 -0700

    page input

---
+++ app/Cow/page.js
@@ -0,0 +1,18 @@
+"use client";
+import CowPage from "./Cow/cow";
+import Moomoo from "./ui/moomoo/moomoo";
+import styles from "@/app/Cow/styles.module.css"
+
+export default function Page() {
+    return (
+        <div>
+        <div className={styles.moo}>
+            <Moomoo
+            size="interaction"
+            src="/moomoonormal.svg"
+            />
+            </div>
+      <CowPage/>
+      </div>
+    );
+}
