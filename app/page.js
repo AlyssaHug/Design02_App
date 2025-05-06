df928b5a68025c4ab8290fc11be3a3144535a833
tc.11927 <tcalaquian2@my.bcit.ca>
2025-05-06 14:25:26 -0700

    moomoo added

--- app/page.js
+++ app/page.js
@@ -1,39 +1,18 @@
 "use client";
-import Coins from "./ui/coins/coins";
-import Nav from "./ui/navbar/navbar";
-import Slider from "@/app/ui/slider/slider";
-
-import styles from "@/app/Cow/styles.module.css";
+import CowPage from "./Cow/cow";
 import Moomoo from "./ui/moomoo/moomoo";
+import styles from "@/app/Cow/styles.module.css"
 
 export default function Page() {
     return (
-        <div className={styles.container}>
-
-            <div className={styles.sliderContainer}>
-                <Slider />
+        <div>
+        <div className={styles.moo}>
+            <Moomoo
+            size="interaction"
+            src="/moomoonormal.svg"
+            />
             </div>
-            <Nav />
-            <div className={styles.sameRow}>
-                <img
-                    src="/left_arrow.svg"
-                    alt="Back arrow"
-                />
-                  <h1 className={styles.header}>MooMoo</h1>
-                <div className={styles.coins}>
-                    <Coins
-                        value="Coins: "
-                        coin={990}
-                    />
-                </div>
-            </div>
-            <div className={styles.background}>
-                <img
-                    src="/grassbg.jpg"
-                    className={styles.backgroundImage}
-                    alt="Grass field background"
-                />
-            </div>
-        </div>
+      <CowPage/>
+      </div>
     );
 }
