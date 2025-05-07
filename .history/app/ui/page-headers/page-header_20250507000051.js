e98488779de77e7ae92ce7e40962b367caf6d7a7
Darrel Soriano <dsoriano5@my.bcit.ca>
2025-05-07 00:06:42 -0700

    Finished Page-Headers

--- .history/app/ui/page-headers/page-header_20250507000051.js
+++ .history/app/ui/page-headers/page-header_20250507000051.js
@@ -6,18 +6,14 @@ const libreFranklin = Libre_Franklin({
     variable: "--font-libre-franklin",
 });
 
-const PageHeader = ({ title, subtitle, icon }) => {
+const PageHeader = ({ title, subtitle, header }) => {
     const headerClasses = classNames(styles.header, libreFranklin.variable, {
         [styles.new_user_homepage_header]: header === "new_user_homepage",
         [styles.your_total_balance]: type === "your_total_balance",
         [styles.balance]: type === "balance",
         [styles.header_title]: type === "header_title",
-        [styles.expense_header]: icon === "expense_header",
-        [styles.goal_homepage_header]: icon === "goal_homepage",
-        [styles.goal_header]: icon === "goal_header",
-        [styles.income_history_header]: icon === "income_history_header",
-        [styles.transactions_this_month_header]: icon === "transactions_this_month_header",
-        [styles.history_subtitle]: icon === "history_subtitle",
+        [styles.expense_header]: icon === "expense",
+        
     });
 
     return (
