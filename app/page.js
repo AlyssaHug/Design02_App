1a0ef03a2a915f8fabd45e8adde70a9b47d8cf2e
tc.11927 <tcalaquian2@my.bcit.ca>
2025-05-07 18:13:04 -0700

    added top bar

--- app/page.js
+++ app/page.js
@@ -1,18 +1,25 @@
 "use client";
 import CowPage from "./Cow/cow";
 import Moomoo from "./ui/moomoo/moomoo";
-import styles from "@/app/Cow/styles.module.css"
+import Coins from "@/app/ui/coins/coins"
+import styles from "@/app/Customize/styles.module.css"
 
 export default function Page() {
     return (
-        <div>
-        <div className={styles.moo}>
-            <Moomoo
-            size="interaction"
-            src="/moomoonormal.svg"
-            />
+       <div>
+         <div className={styles.sameRow}>
+                <img
+                    src="/left_arrow.svg"
+                    alt="Back arrow"
+                />
+                  <h1 className={styles.header}>Customize</h1>
+                <div className={styles.coins}>
+                    <Coins
+                        value="Coins: "
+                        coin={990}
+                    />
+                </div>
             </div>
-      <CowPage/>
-      </div>
+       </div>
     );
 }
