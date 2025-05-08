c6f0f2a576875801047dade1a6c1732becef2a10
tc.11927 <tcalaquian2@my.bcit.ca>
2025-05-07 19:22:00 -0700

    put moomoo in his place :D

--- app/page.js
+++ app/page.js
@@ -1,18 +1,18 @@
 "use client";
 import CowPage from "./Cow/cow";
 import Moomoo from "./ui/moomoo/moomoo";
-import Coins from "@/app/ui/coins/coins"
-import styles from "@/app/Customize/styles.module.css"
+import Coins from "@/app/ui/coins/coins";
+import styles from "@/app/Customize/styles.module.css";
 
 export default function Page() {
     return (
-       <div>
-         <div className={styles.sameRow}>
+        <div>
+            <div className={styles.sameRow}>
                 <img
                     src="/left_arrow.svg"
                     alt="Back arrow"
                 />
-                  <h1 className={styles.header}>Customize</h1>
+                <h1 className={styles.header}>Customize</h1>
                 <div className={styles.coins}>
                     <Coins
                         value="Coins: "
@@ -20,6 +20,17 @@ export default function Page() {
                     />
                 </div>
             </div>
-       </div>
+            <div className={styles.cowContainer}>
+                <div className={styles.cowback}></div>
+
+                <div className={styles.mooplace}>
+                    <Moomoo
+                        size="dressup"
+                        src="/moomoonormal.svg"
+                        className={styles.moomooSvg}
+                    />
+                </div>
+            </div>
+        </div>
     );
 }
