6d417dd38378ae681d1f017d8eaf162855f5d197
AlyssaHug <AlyssaHug@shaw.ca>
2025-05-11 19:04:06 -0700

    added settings button component in Button module

--- app/Profile/page.js
+++ app/Profile/page.js
@@ -1,10 +1,16 @@
-import Nav from "../ui/navbar/navbar";
+import Nav from "@/app/ui/navbar/navbar";
+import { SettingButton } from "@/app/ui/Buttons/Buttons";
 export default function Page() {
     return (
         <div>
             <Nav />
             <h1>Hi there user!</h1>
             <div></div>
+            <SettingButton
+                value='Settings'
+                imageSrc='right_arrow.svg'
+                type='settings'
+            />
         </div>
     );
 }
