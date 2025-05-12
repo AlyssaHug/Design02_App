28dd87e39b2728952b8cd8a78ed9b08fd450eba4
AlyssaHug <AlyssaHug@shaw.ca>
2025-05-11 18:49:51 -0700

    started account homepage

---
+++ app/Profile/page.js
@@ -0,0 +1,10 @@
+import Nav from "../ui/navbar/navbar";
+export default function Page() {
+    return (
+        <div>
+            <Nav />
+            <h1>Hi there user!</h1>
+            <div></div>
+        </div>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -2,17 +2,21 @@
 import Nav from "./ui/navbar/navbar";
 import GoalProgress from "./ui/GoalCards/GoalCards";
 import Button from "./ui/Buttons/Buttons";
+import TextField from "./ui/TextField/TextField";
 export default function Page() {
     return (
-        <div className={styles.container}>
+        <div>
             <Nav />
             <Button
                 color='light'
                 value='da shop'
                 href='/Cow/Shop'
             />
+            <Button
+                value='Settings'
+                imageSrc='/right_arrow.svg'
+            />
             <GoalProgress />
         </div>
     );
 }
-
